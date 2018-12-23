6c1ece669863a3a9afa5f5a67c84820e26a97bdf
miao shan <minnieshan@gmail.com>
2018-12-23 21:33:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
+//function sayHiToGrandma(string){
 
 if(string.toLowerCase()===lowercase){
   return "I can't hear you!"
